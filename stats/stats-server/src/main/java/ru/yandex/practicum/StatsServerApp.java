9715e8a628686a4fcaa1bec3a0132a9d7a4a5e17
ea7hie <ea7hie@mail.ru>
2025-07-06 12:13:26 +0300

    Добавлен SpringBootApplication в main-service. Диплом. Этап 1. Ревью 1.

--- stats/stats-server/src/main/java/ru/yandex/practicum/StatsServerApp.java
+++ stats/stats-server/src/main/java/ru/yandex/practicum/StatsServerApp.java
@@ -1,17 +1,11 @@
 package ru.yandex.practicum;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
 @SpringBootApplication
-@Component
-@Data
-@AllArgsConstructor
 public class StatsServerApp {
     public static void main(String[] args) {
         SpringApplication.run(StatsServerApp.class, args);
