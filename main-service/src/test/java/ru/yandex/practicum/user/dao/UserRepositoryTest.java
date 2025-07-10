007c2b11f4751a0951a1af28df2790ddc86f9309
ea7hie <ea7hie@mail.ru>
2025-07-10 13:11:52 +0300

    Написаны тесты на сущность User (административное право доступа). Диплом. Этап 2. Ревью 1.

--- main-service/src/test/java/ru/yandex/practicum/user/dao/UserRepositoryTest.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.user.dao;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserRepositoryTest {
-
-}
--- main-service/src/test/java/ru/yandex/practicum/user/dto/UserShortDtoTest.java
+++ main-service/src/test/java/ru/yandex/practicum/user/dto/UserShortDtoTest.java
@@ -1,7 +1,46 @@
 package ru.yandex.practicum.user.dto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class UserShortDtoTest {
 
+    @Test
+    void testNoArgsConstructor() {
+        var dto = new UserShortDto();
+        assertThat(dto.getId()).isNull();
+        assertThat(dto.getName()).isNull();
+    }
+
+    @Test
+    void testAllArgsConstructor() {
+        var dto = new UserShortDto(123L, "Test Name");
+        assertThat(dto).usingRecursiveComparison()
+                .isEqualTo(new UserShortDto(123L, "Test Name"));
+    }
+
+    @Test
+    void testSettersAndGetters() {
+        var dto = new UserShortDto();
+
+        dto.setId(456L);
+        dto.setName("Another Name");
+
+        assertThat(dto).usingRecursiveComparison()
+                .isEqualTo(new UserShortDto(456L, "Another Name"));
+    }
+
+    @Test
+    void testEqualsAndHashCode() {
+        var dto1 = new UserShortDto(789L, "User");
+        var dto2 = new UserShortDto(789L, "User");
+        var differentId = new UserShortDto(999L, "User");
+        var differentName = new UserShortDto(789L, "Another");
+
+        assertThat(dto1).isEqualTo(dto2);
+        assertThat(dto1).isNotEqualTo(differentId);
+        assertThat(dto1).isNotEqualTo(differentName);
+        assertThat(dto1.hashCode()).isEqualTo(dto2.hashCode());
+    }
 }
