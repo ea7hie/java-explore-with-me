9501a93e0a314f9d0d121cdadc8ddbd6ffcb9c51
ea7hie <ea7hie@mail.ru>
2025-07-15 20:56:36 +0300

    Исправления по коду после запуска постман-тестов из гитхаба локально. Часть 3. Диплом. Этап 2. Ревью 1.

--- stats/stats-dto/src/main/java/ru/yandex/practicum/StatisticDtoGet.java
+++ stats/stats-dto/src/main/java/ru/yandex/practicum/StatisticDtoGet.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Objects;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,12 +11,4 @@ public class StatisticDtoGet {
     private String app;
     private String uri;
     private Long hits;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        StatisticDtoGet dto = (StatisticDtoGet) o;
-        return Objects.equals(app, dto.app) && Objects.equals(uri, dto.uri);
-    }
 }
